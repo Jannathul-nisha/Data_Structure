19d9ac9e58199cf4de3ebb8e09812195e057a26c
JANNATHUL NISHA S <153101826+Jannathul-nisha@users.noreply.github.com>
2025-06-28 14:01:54 +0530

    Add files via upload

---
+++ Shift_Node.c
@@ -0,0 +1,58 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct Node {
+int data;
+struct Node* next;
+};
+struct Node* head=NULL;
+void insertAtEnd(int value) {
+struct Node* newNode=(struct Node*)malloc(sizeof(struct Node));
+struct Node* temp=head;
+newNode->data=value;
+newNode->next=NULL;
+if(head==NULL) {
+head=newNode;
+}
+else {
+while(temp->next!=NULL) {
+temp=temp->next;
+}
+temp->next=newNode;
+
+}
+}
+void display() {
+struct Node* temp=head;
+while(temp!=NULL) {
+printf("Element:%d\n",temp->data);
+temp=temp->next;
+}
+printf("NULL\n");
+}
+void shift(int val) {
+    struct Node* temp=head;
+    while(val){
+    while(temp->next!=NULL){
+        temp=temp->next;
+    }
+    temp->next=head;
+    head=temp;
+    struct Node* last=head;
+    while(last->next!=head){
+        last=last->next;
+    }
+    last->next=NULL;
+    val--;
+    }
+}
+int main() {
+insertAtEnd(10);
+insertAtEnd(20);
+insertAtEnd(30);
+insertAtEnd(40);
+insertAtEnd(50);
+display();
+shift(3);
+display();
+return 0;
+}
